d272a208b67fe83d99fccd1e8f3452a1d234dda3
Seva <20211206nch@mail.ru>
2026-06-09 23:07:33 +0300

    fix: added tsq query in mainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+
 export const MainPage = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json()
+      ),
+  });
+  console.log(data);
   return <div>MainPage</div>;
 };
