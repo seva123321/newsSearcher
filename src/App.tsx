923183994f787d19e78ef2c53f4aec253f2d1bb2
Seva <20211206nch@mail.ru>
2026-06-09 21:31:56 +0300

    create config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+// import heroImg from './assets/hero.png'
+{/* <img src={heroImg} className="base" width="170" height="179" alt="" /> */}
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
