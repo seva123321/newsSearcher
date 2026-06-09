b339dd4d59cfc2079dbd84fdbbfce7b620d5fb99
Seva <20211206nch@mail.ru>
2026-06-09 23:03:03 +0300

    feat: added alias path

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { StyleProvider } from '@ant-design/cssinjs';
 import { DatePicker, ConfigProvider } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MainPage } from '@/pages';
 
+const queryClient = new QueryClient();
 const { RangePicker } = DatePicker;
 
 function App() {
   return (
     <StyleProvider layer>
       <ConfigProvider>
-        <RangePicker showTime className="border-orange-500 shadow-lg" />
-        <div
-          style={{
-            backgroundColor: 'var(--color-bg)',
-            color: 'var(--color-text)',
-          }}
-        >
-          <RangePicker className="!bg-[var(--color-bg)]" />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <RangePicker showTime className="border-orange-500 shadow-lg" />
+          <div
+            style={{
+              backgroundColor: 'var(--color-bg)',
+              color: 'var(--color-text)',
+            }}
+          >
+            <MainPage />
+            <RangePicker className="!bg-[var(--color-bg)]" />
+          </div>
+        </QueryClientProvider>
       </ConfigProvider>
     </StyleProvider>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
