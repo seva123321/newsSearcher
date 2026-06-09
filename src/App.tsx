999ff86c8fe01c0e080672920500192c2278117b
Seva <20211206nch@mail.ru>
2026-06-09 23:55:11 +0300

    fix: useed ThemeCard in MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import { StyleProvider } from '@ant-design/cssinjs';
-import { DatePicker, ConfigProvider } from 'antd';
+import { ConfigProvider } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainPage } from '@/pages';
 
 const queryClient = new QueryClient();
-const { RangePicker } = DatePicker;
 
 function App() {
   return (
     <StyleProvider layer>
       <ConfigProvider>
         <QueryClientProvider client={queryClient}>
-          <RangePicker showTime className="border-orange-500 shadow-lg" />
-          <div
-            style={{
-              backgroundColor: 'var(--color-bg)',
-              color: 'var(--color-text)',
-            }}
-          >
-            <MainPage />
-            <RangePicker className="!bg-[var(--color-bg)]" />
-          </div>
+          <MainPage />
         </QueryClientProvider>
       </ConfigProvider>
     </StyleProvider>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,14 @@
+import ThemeCard from '@/components/ThemeCard';
 import { useQuery } from '@tanstack/react-query';
+import { DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
+import { Layout } from 'antd';
+import { useState } from 'react';
+
+const { Header, Footer, Content } = Layout;
 
 export const MainPage = () => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
   const { isPending, error, data } = useQuery({
     queryKey: ['repoData'],
     queryFn: () =>
@@ -8,6 +16,76 @@ export const MainPage = () => {
         res.json()
       ),
   });
+
+  const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 48,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+  };
+
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#0958d9',
+  };
+
+  const layoutStyle: React.CSSProperties = {
+    borderRadius: 8,
+    overflow: 'hidden',
+    width: '100%',
+    maxWidth: '100%',
+  };
   console.log(data);
-  return <div>MainPage</div>;
+
+  const dataNews = {
+    theme: 'БПЛА в Москве',
+    news: [
+      {
+        tlg_chanel: '@baza2',
+        date: '2026-06-10T09:15:12.155Z',
+        link: 'https://t.baza2/6567326523',
+        text: 'БПЛА сегодня летали в Москве',
+      },
+      {
+        tlg_chanel: '@baza2',
+        date: '2026-06-10T09:15:12.155Z',
+        link: 'https://t.baza2/6567326523',
+        text: 'БПЛА сегодня летали в Москве',
+      },
+      {
+        tlg_chanel: '@baza2',
+        date: '2026-06-10T09:15:12.155Z',
+        link: 'https://t.baza2/6567326523',
+        text: 'БПЛА сегодня летали в Москве',
+      },
+      {
+        tlg_chanel: '@baza2',
+        date: '2026-06-10T09:15:12.155Z',
+        link: 'https://t.baza2/6567326523',
+        text: 'БПЛА сегодня летали в Москве',
+      },
+    ],
+  };
+
+  return (
+    <Layout style={layoutStyle}>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>
+          {/* <RangePicker showTime className="border-orange-500 shadow-lg" /> */}
+          <RangePicker showTime className="!bg-[var(--color-bg)]" />
+          <ThemeCard
+            theme={dataNews.theme}
+            news={dataNews.news}
+            onToggleCollapse={(newCollapsed) => setCollapsed(newCollapsed)}
+          />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
