b11ad764f4e6f5a0630a16eed6b7e271661bbf47
Seva <20211206nch@mail.ru>
2026-06-10 22:07:35 +0300

    fix: added locale antd with dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { StyleProvider } from '@ant-design/cssinjs';
 import { ConfigProvider } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainPage } from '@/pages';
+import locale from 'antd/locale/ru_RU';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 
+// 1. Устанавливаем русскую локаль для dayjs (библиотека для работы с датами внутри Ant Design)
+dayjs.locale('ru');
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <StyleProvider layer>
-      <ConfigProvider>
+      <ConfigProvider locale={locale}>
         <QueryClientProvider client={queryClient}>
           <MainPage />
         </QueryClientProvider>
