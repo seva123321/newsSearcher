708db8ec727800dd5ec9cd3ea97987ece8ca7439
Seva <20211206nch@mail.ru>
2026-06-13 19:57:57 +0300

    feat: created config for url

---
+++ src/types/config.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  BASE_URL: 'https://localhost:5173',
+  PORT: 5173,
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from '@/types/typesNews';
 export * from '@/types/typesTg';
 export * from '@/types/typesUI';
+export * from '@/types/config';
